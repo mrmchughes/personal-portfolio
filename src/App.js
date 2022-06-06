31d8dc08318ea34ba9404c6cce3aebf9a7ba3a23
Michael <mrmchughes27@gmail.com>
2022-06-06 15:36:28 -0700

    rough layout completed, css finetuning needed

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
+import Icon from "@mdi/react";
+import { mdiArrowTopRightBoldBox } from "@mdi/js";
+
 function App() {
   return (
     <div className="App font-serif">
-      <section className="flex flex-row justify-center">
+      <section className="flex flex-row flex-wrap justify-center p-2.5">
         <img
           src={require("./imgs/profile image.png")}
           alt="Michael Hughes"
-          className="object-contain h-48 w-96"
+          className="object-contain min-h-48 max-w-sm mr-2.5"
         />
         <div className="">
-          <h2>About</h2>
-          <p className="mt-2.5">
+          <p className="text-xl">About me</p>
+          <p className="mt-2.5 max-w-md">
             During a time when I was feeling dissatisfied in my previous career,
             I began coding by working my way through a beginner’s book on
             Python. I knew even then I had found something I was truly
@@ -106,212 +109,198 @@ function App() {
         </div>
       </section>
 
-      <section className="grid gap-2.5 grid-cols-auto-fit">
-        <h2>My work</h2>
-        <div className="border-solid border border-black">
-          <div>
-            <img
-              src="https://user-images.githubusercontent.com/10659805/153058789-c29aa45f-28d7-4bbc-8905-9ebbfa880e4d.png"
-              alt="Screenshot of Memory Card Project"
-            />
-            <div>
-              <p>Memory Card</p>
+      <section className="flex justify-center">
+        <div>
+          <p className="text-xl">My work</p>
+
+          <div className="grid gap-3 grid-cols-auto-fill mt-2.5">
+            <div className="border-solid border border-black p-2.5">
+              <img
+                src="https://user-images.githubusercontent.com/10659805/153058789-c29aa45f-28d7-4bbc-8905-9ebbfa880e4d.png"
+                alt="Screenshot of Memory Card Project"
+                className="w-full h-36 mb-2"
+              />
               <div>
-                <a href="https://github.com/mrmchughes/Memory-Card">
-                  <img
-                    src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg"
-                    alt="Github Repository"
-                    className="h-8 w-8"
-                  />
-                </a>
-                <a href="https://mrmchughes.github.io/Memory-Card/">
-                  <img
-                    src={require("./imgs/arrow-top-right-bold-box-outline.svg")}
-                    alt="Live Preview"
-                  />
-                </a>
+                <div className="flex flex-row space-x-4">
+                  <p className="mr-2.5">Memory Card</p>
+                  <a href="https://github.com/mrmchughes/Memory-Card">
+                    <img
+                      src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg"
+                      alt="Github Repository"
+                      className="h-8 w-8"
+                    />
+                  </a>
+                  <a href="https://mrmchughes.github.io/Memory-Card/">
+                    <Icon path={mdiArrowTopRightBoldBox} size={1} />
+                  </a>
+                </div>
+                <p className="mt-2.5">
+                  Developed a Card Memory game, where players click on a card in
+                  an array, and attempt to remember which ones they have already
+                  selected.
+                </p>
+                <p className="mt-2.5">
+                  Built With: HTML | CSS | JavaScript | React
+                </p>
               </div>
-              <p className="mt-2.5">
-                Developed a Card Memory game, where players click on a card in
-                an array, and attempt to remember which ones they have already
-                selected.
-              </p>
-              <p className="mt-2.5">
-                Built With: HTML | CSS | JavaScript | React
-              </p>
             </div>
-          </div>
-        </div>
-        <div className="border-solid border border-black">
-          <div>
-            <img
-              src="https://user-images.githubusercontent.com/10659805/172234215-bb18174b-da8a-4b6e-aa1f-c41522c51b79.png"
-              alt="Screenshot of CV-Application Project"
-            />
-            <div>
-              <p>CV-Application</p>
+
+            <div className="border-solid border border-black p-2.5">
+              <img
+                src="https://user-images.githubusercontent.com/10659805/172234215-bb18174b-da8a-4b6e-aa1f-c41522c51b79.png"
+                alt="Screenshot of CV-Application Project"
+                className="w-full h-36 mb-2"
+              />
               <div>
-                <a href="https://github.com/mrmchughes/CV-Application">
-                  <img
-                    src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg"
-                    alt="Github Repository"
-                    className="h-8 w-8"
-                  />
-                </a>
-                <a href="https://mrmchughes.github.io/CV-Application">
-                  <img
-                    src={require("./imgs/arrow-top-right-bold-box-outline.svg")}
-                    alt="Live Preview"
-                  />
-                </a>
+                <div className="flex flex-row space-x-4">
+                  <p className="mr-2.5">CV-Application</p>
+                  <a href="https://github.com/mrmchughes/CV-Application">
+                    <img
+                      src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg"
+                      alt="Github Repository"
+                      className="h-8 w-8"
+                    />
+                  </a>
+                  <a href="https://mrmchughes.github.io/CV-Application">
+                    <Icon path={mdiArrowTopRightBoldBox} size={1} />
+                  </a>
+                </div>
+                <p className="mt-2.5">
+                  Developed a React application for users to input their
+                  information, and using the user's information, dynamically
+                  populate a CV.
+                </p>
+                <p className="mt-2.5">
+                  Built With: HTML | CSS | JavaScript | React
+                </p>
               </div>
-              <p className="mt-2.5">
-                Developed a React application for users to input their
-                information, and using the user's information, dynamically
-                populate a CV.
-              </p>
-              <p className="mt-2.5">
-                Built With: HTML | CSS | JavaScript | React
-              </p>
             </div>
-          </div>
-        </div>
-        <div className="border-solid border border-black">
-          <div>
-            <img
-              src="https://user-images.githubusercontent.com/10659805/127752181-0bf3586a-fa60-4eb8-888b-d66c3b30b4bd.png"
-              alt="Screenshot of Odins_Operations Project"
-            />
-            <div>
-              <p>Odins_Operations</p>
+
+            <div className="border-solid border border-black p-2.5">
+              <img
+                src="https://user-images.githubusercontent.com/10659805/127752181-0bf3586a-fa60-4eb8-888b-d66c3b30b4bd.png"
+                alt="Screenshot of Odins_Operations Project"
+                className="w-full h-36 mb-2"
+              />
               <div>
-                <a href="https://github.com/mrmchughes/odins_operations">
-                  <img
-                    src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg"
-                    alt="Github Repository"
-                    className="h-8 w-8"
-                  />
-                </a>
-                <a href="https://mrmchughes.itch.io/odins-operations">
-                  <img
-                    src={require("./imgs/arrow-top-right-bold-box-outline.svg")}
-                    alt="Live Preview"
-                  />
-                </a>
+                <div className="flex flex-row space-x-4">
+                  <p className="mr-2.5">Odins_Operations</p>
+                  <a href="https://github.com/mrmchughes/odins_operations">
+                    <img
+                      src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg"
+                      alt="Github Repository"
+                      className="h-8 w-8"
+                    />
+                  </a>
+                  <a href="https://mrmchughes.itch.io/odins-operations">
+                    <Icon path={mdiArrowTopRightBoldBox} size={1} />
+                  </a>
+                </div>
+                <p className="mt-2.5">
+                  A group project developed for The Odin Project July 2021 game
+                  jam, inspired by the classic edutainment game Number Munchers.
+                </p>
+                <p className="mt-2.5">Built With: HTML | CSS | JavaScript</p>
               </div>
-              <p className="mt-2.5">
-                A group project developed for The Odin Project July 2021 game
-                jam, inspired by the classic edutainment game Number Munchers.
-              </p>
-              <p className="mt-2.5">Built With: HTML | CSS | JavaScript</p>
             </div>
-          </div>
-        </div>
-        <div className="border-solid border border-black">
-          <div>
-            <img
-              src="https://user-images.githubusercontent.com/10659805/172234883-f6d0bd42-cd2d-4efc-a971-732cb0f855b7.png"
-              alt="Screenshot of Todo-List Project"
-            />
-            <div>
-              <p>Todo-List</p>
+
+            <div className="border-solid border border-black p-2.5">
+              <img
+                src="https://user-images.githubusercontent.com/10659805/172234883-f6d0bd42-cd2d-4efc-a971-732cb0f855b7.png"
+                alt="Screenshot of Todo-List Project"
+                className="w-full h-36 mb-2"
+              />
               <div>
-                <a href="https://github.com/mrmchughes/todo-list-project-2">
-                  <img
-                    src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg"
-                    alt="Github Repository"
-                    className="h-8 w-8"
-                  />
-                </a>
-                <a href="https://mrmchughes.github.io/todo-list-project-2/">
-                  <img
-                    src={require("./imgs/arrow-top-right-bold-box-outline.svg")}
-                    alt="Live Preview"
-                  />
-                </a>
+                <div className="flex flex-row space-x-4">
+                  <p className="mr-2.5">Todo-List</p>
+                  <a href="https://github.com/mrmchughes/todo-list-project-2">
+                    <img
+                      src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg"
+                      alt="Github Repository"
+                      className="h-8 w-8"
+                    />
+                  </a>
+                  <a href="https://mrmchughes.github.io/todo-list-project-2/">
+                    <Icon path={mdiArrowTopRightBoldBox} size={1} />
+                  </a>
+                </div>
+                <p className="mt-2.5">
+                  Developed a Todo-List application for users to create Todo
+                  Items and Project Folders.
+                </p>
+                <p className="mt-2.5">
+                  Built With: HTML | CSS | JavaScript | Webpack | Local Storage
+                </p>
               </div>
-              <p className="mt-2.5">
-                Developed a Todo-List application for users to create Todo Items
-                and Project Folders.
-              </p>
-              <p className="mt-2.5">
-                Built With: HTML | CSS | JavaScript | Webpack | Local Storage
-              </p>
             </div>
-          </div>
-        </div>
-        <div className="border-solid border border-black">
-          <div>
-            <img
-              src="https://user-images.githubusercontent.com/10659805/172235150-a11b8184-580b-4b18-8edf-f24dfd18770b.png"
-              alt="Screenshot of Weather App Project"
-            />
-            <div>
-              <p>Weather App</p>
+
+            <div className="border-solid border border-black p-2.5">
+              <img
+                src="https://user-images.githubusercontent.com/10659805/172235150-a11b8184-580b-4b18-8edf-f24dfd18770b.png"
+                alt="Screenshot of Weather App Project"
+                className="w-full h-36 mb-2"
+              />
               <div>
-                <a href="https://github.com/mrmchughes/Weather-App">
-                  <img
-                    src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg"
-                    alt="Github Repository"
-                    className="h-8 w-8"
-                  />
-                </a>
-                <a href="https://mrmchughes.github.io/Weather-App/">
-                  <img
-                    src={require("./imgs/arrow-top-right-bold-box-outline.svg")}
-                    alt="Live Preview"
-                  />
-                </a>
+                <div className="flex flex-row space-x-4">
+                  <p className="mr-2.5">Weather App</p>
+                  <a href="https://github.com/mrmchughes/Weather-App">
+                    <img
+                      src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg"
+                      alt="Github Repository"
+                      className="h-8 w-8"
+                    />
+                  </a>
+                  <a href="https://mrmchughes.github.io/Weather-App/">
+                    <Icon path={mdiArrowTopRightBoldBox} size={1} />
+                  </a>
+                </div>
+                <p className="mt-2.5">
+                  Developed an application that allows for users to input a
+                  location, and with the use of the Open Weather API, receive
+                  the searched location's current forecast.
+                </p>
+                <p className="mt-2.5">
+                  Built With: HTML | CSS | JavaScript | Git
+                </p>
               </div>
-              <p className="mt-2.5">
-                Developed an application that allows for users to input a
-                location, and with the use of the Open Weather API, receive the
-                searched location's current forecast.
-              </p>
-              <p className="mt-2.5">
-                Built With: HTML | CSS | JavaScript | Git
-              </p>
             </div>
-          </div>
-        </div>
-        <div className="border-solid border border-black">
-          <div>
-            <img
-              src="https://user-images.githubusercontent.com/10659805/164803283-17f48e5d-c2cd-4a71-b931-6778d206e1e1.png"
-              alt="Screenshot of Project"
-            />
-            <div>
-              <p>Shopping Cart</p>
+
+            <div className="border-solid border border-black p-2.5">
+              <img
+                src="https://user-images.githubusercontent.com/10659805/164803283-17f48e5d-c2cd-4a71-b931-6778d206e1e1.png"
+                alt="Screenshot of Project"
+                className="w-full h-36 mb-2"
+              />
               <div>
-                <a href="https://github.com/mrmchughes/shopping-cart">
-                  <img
-                    src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg"
-                    alt="Github Repository"
-                    className="h-8 w-8"
-                  />
-                </a>
-                <a href="https://mrmchughes.github.io/shopping-cart/">
-                  <img
-                    src={require("./imgs/arrow-top-right-bold-box-outline.svg")}
-                    alt="Live Preview"
-                  />
-                </a>
+                <div className="flex flex-row space-x-4">
+                  <p className="mr-2.5">Shopping Cart</p>
+                  <a href="https://github.com/mrmchughes/shopping-cart">
+                    <img
+                      src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg"
+                      alt="Github Repository"
+                      className="h-8 w-8"
+                    />
+                  </a>
+                  <a href="https://mrmchughes.github.io/shopping-cart/">
+                    <Icon path={mdiArrowTopRightBoldBox} size={1} />
+                  </a>
+                </div>
+                <p className="mt-2.5">
+                  Developed an E-Commerce site, incorporating Routes with
+                  react-router-dom.
+                </p>
+                <p className="mt-2.5">
+                  Built with: HTML | CSS | JavaScript | React | NPM | Tailwind
+                  CSS | TypeScript
+                </p>
               </div>
-              <p className="mt-2.5">
-                Developed an E-Commerce site, incorporating Routes with
-                react-router-dom.
-              </p>
-              <p className="mt-2.5">
-                Built with: HTML | CSS | JavaScript | React | NPM | Tailwind CSS
-                | TypeScript
-              </p>
             </div>
           </div>
         </div>
       </section>
 
-      <section className="">
-        <h2>Contact me</h2>
+      <section className="flex flex-col flex-wrap justify-center p-2.5 mt-2.5">
+        <p className="text-xl">Contact me</p>
         <p className="mt-2.5">
           Please get in touch if you think our work could be mutually
           beneficial.
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,11 +4,11 @@ module.exports = {
     extend: {
       gridTemplateColumns: {
         "auto-fit": "repeat(auto-fit, minmax(0, 1fr))",
-        "auto-fill": "repeat(auto-fit, minmax(0, 1fr))",
+        "auto-fill": "repeat(auto-fill, minmax(250px, 1fr))",
       },
       gridTemplateRows: {
         "auto-fit": "repeat(auto-fit, minmax(0, 1fr))",
-        "auto-fill": "repeat(auto-fit, minmax(0, 1fr))",
+        "auto-fill": "repeat(auto-fill, minmax(0, 1fr))",
       },
     },
   },
